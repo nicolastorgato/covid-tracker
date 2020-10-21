9bc31c1cb7d3eb7648967027d71e1a5d09c9e3ea
storgatonicola <storgatonicola@provincia.treviso.it>
2020-10-21 13:40:49 +0200

    add countryPicker, barChart, more styling

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,17 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api'; 
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+
+    let changebleUrl = url;
+
+    if (country) {
+        changebleUrl = `${url}/countries/${country}`;
+    }
+
     try {
         // data is destructured from response
-        const { data: { confirmed, recovered, deaths, lastUpdate} } = await axios.get(url);  
+        const { data: { confirmed, recovered, deaths, lastUpdate} } = await axios.get(changebleUrl);  
 
         // quando il nome della proprietà dell'oggetto è uguale al suo valore, 
         // basta scrivere il nome della proprietà
@@ -19,7 +26,7 @@ export const fetchData = async () => {
         return modifiedData;
 
     } catch (error) {
-        
+        console.log(error);
     }
 }
 
@@ -36,6 +43,17 @@ export const fetchDailyData = async () => {
 
         return modifiedData;
     } catch (error) {
-        
+         console.log(error);
+    }
+}
+
+
+export const fetchCountries = async () => {
+    try {
+         const {data : {countries}} = await axios.get(`${url}/countries`);
+         return countries.map( (country) =>  country.name);
+         
+    } catch (error) {
+        console.log(error);
     }
 }
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { NativeSelect, FormControl} from '@material-ui/core';
+import {fetchCountries} from '../../api';
 
-const CountryPicker = () => {
+import styles from './CountryPicker.module.css';
+
+const CountryPicker = ({handleCountryChange}) => {
+
+    const [fetchedCountries, setFetchedCountries] = useState([]);
+
+    useEffect( () => {
+        const fetchAPI = async () => {
+            setFetchedCountries( await fetchCountries() );
+        }
+
+        fetchAPI();
+    }, [fetchedCountries]);
+
     return (
-        <h1> CountryPicker</h1>
+        <FormControl className={styles.formControl}>
+            <NativeSelect defaultValue="" onChange={ (e) => handleCountryChange(e.target.value)}>
+                <option value=""> Global </option>
+                { fetchedCountries.map( (country, index) => <option key={index} value={country}> {country} </option> ) }
+            </NativeSelect>
+        </FormControl>
     );
 }
 
